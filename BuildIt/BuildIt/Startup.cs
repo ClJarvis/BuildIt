3dde10131373d177f4bd88f7970aea8f934d7ee8
Chris_Jarvis <clj@ollure.com>
2015-11-21 11:04:27 -0600

    inital files

---
+++ BuildIt/BuildIt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BuildIt.Startup))]
+namespace BuildIt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
