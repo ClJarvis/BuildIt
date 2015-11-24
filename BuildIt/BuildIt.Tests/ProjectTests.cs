5d8c7103b6f633c112156e1cbbab50a994c0dd15
Chris_Jarvis <clj@ollure.com>
2015-11-23 18:44:33 -0600

    created project tests

---
+++ BuildIt/BuildIt.Tests/ProjectTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BuildIt.Models;
+
+namespace BuildIt.Tests
+{
+    [TestClass]
+    public class ProjectTests
+    {
+        [TestMethod]
+        public void EnsureICanCreatAnInstanceAProject()
+        {
+            Project project = new Project();
+            Assert.IsNotNull(project);
+        }
+
+        [TestMethod]
+        public void EnsureProjectPropertiesWork()
+        {
+            Project project = new Project { Name = "My First Project" };
+
+
+            Assert.AreEqual("My First Project", project.Name);
+
+        }
+    }
+}
---
+++ BuildIt/BuildIt/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuildIt.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string Name { get; set; }
+        public int UserID { get; set; }
+        public DateTime StartDate { get; set; }
+        
+    }
+}
