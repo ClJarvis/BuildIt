0ff56c02910090bfd24990fb3dc4d6c88cd99fb7
Chris_Jarvis <clj@ollure.com>
2015-11-26 15:28:02 -0600

    building main inventory

--- BuildIt/BuildIt/Models/Inventory.cs
+++ BuildIt/BuildIt/Models/Inventory.cs
@@ -7,8 +7,15 @@ namespace BuildIt.Models
 {
     public class Inventory
     {
+        private   InventoryContent content;
+
         public String FabricName { get; set; }
         public int FabricAmount { get; set; }
         public String FabricColor { get; set; }
+
+        public Inventory()
+        {
+            content = new InventoryContent();
+        }
     }
 }
---
+++ BuildIt/BuildIt/Models/InventoryContent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BuildIt.Models
+{
+    
+        public class InventoryContent : DbContext
+        { 
+        public virtual DbSet<Inventory> Inventory { get; set; }
+        }
+    
+}
