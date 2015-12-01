58b8f183ad870963542491a619b7a4edc988f72d
Chris_Jarvis <clj@ollure.com>
2015-12-01 16:30:19 -0600

    Seperated project and Inventory tests

---
+++ BuildIt/BuildIt.Tests/InventoryTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BuildIt.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
