3de4fa105489e9545aa70652fb13d54c7cdae05d
Chris_Jarvis <clj@ollure.com>
2015-12-19 20:53:07 -0600

    edited foreach

--- BuildIt/BuildIt/DAL/InventoryInitializer.cs
+++ BuildIt/BuildIt/DAL/InventoryInitializer.cs
@@ -29,7 +29,7 @@ namespace BuildIt.DAL
 
               
                 {
-                new Project {ProjectId=0001, ProjectName="Dark Brown", /*Owner=6,*/ StartDate= DateTime.Today, /* datetime now? *ProjectItems= List<Inventory>*/},
+                new Project {ProjectId=0001, ProjectName="Dark Brown", /*Owner=6,*/ StartDate= DateTime.Now, /* datetime now? *ProjectItems= List<Inventory>*/},
                
 
                 };
